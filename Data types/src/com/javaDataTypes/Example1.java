7c14cdd6431136d3b62534404293fcd08eeb179a
admin <admin@DESKTOP-L61T1KU>
2022-02-09 12:09:55 +0530

    first commit

---
+++ Data types/src/com/javaDataTypes/Example1.java
@@ -0,0 +1,10 @@
+package com.javaDataTypes;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		int age=24;
+		System.out.println(age);
+	}
+
+}
---
+++ Data types/src/com/javaDataTypes/Example2.java
@@ -0,0 +1,23 @@
+package com.javaDataTypes;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		
+		//int ages=24,28,25;
+	  //System.out.println(ages);
+	
+	int ages[]= {24,28,25};
+	
+	//System.out.println(age[]);
+	
+	for(int age: ages)
+	{
+		System.out.println(age);
+	}
+	
+	
+
+	}
+
+}
